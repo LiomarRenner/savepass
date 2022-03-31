51921ab9c2fe9d1eb38700436420a1c6e4196839
liomar.rabelo <liomar.rabelo@compasso.com.br>
2022-03-31 16:49:19 -0300

    Update Form Index

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import uuid from 'react-native-uuid';
@@ -10,7 +10,21 @@ import { Button } from '../../components/Button';
 import { HeaderForm } from '../../components/HeaderForm';
 
 export function Form() {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
+  function handleNew(){
+    const id = uuid.v4();
+
+    const newData = {
+      id,
+      name,
+      user,
+      password,
+    }
+
+  }
 
   return (
     <KeyboardAvoidingView
@@ -25,20 +39,24 @@ export function Form() {
           <View style={styles.form}>
             <Input
               label="Nome do serviço"
+              onChangeText={setName}
             />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
             />
             <Input
               label="Senha"
               secureTextEntry
+              onChangeText={setPassword}
             />
           </View>
 
           <View style={styles.footer}>
             <Button
               title="Salvar"
+              onPress={handleNew}
             />
           </View>
         </ScrollView>
